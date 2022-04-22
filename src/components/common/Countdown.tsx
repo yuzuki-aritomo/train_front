a931eccb6304e044fa605bb787bdef04a83ca127
taisei yamashita <taisei6514@gmail.com>
2022-04-22 23:19:14 +0900

    スタイルの修正

--- src/components/common/Countdown.tsx
+++ src/components/common/Countdown.tsx
@@ -3,15 +3,13 @@ import styled from 'styled-components';
 
 export const Countdown: React.FC = () => {
   return (
-    <div>
-      <CountdownWrapper>
-        <Description>出発時刻まで残り...</Description>
-        <RemainingTime>10:30</RemainingTime>
-        <DepartureTime>
-          09:39 <span>発</span>
-        </DepartureTime>
-      </CountdownWrapper>
-    </div>
+    <CountdownWrapper>
+      <Description>出発時刻まで残り...</Description>
+      <RemainingTime>10:30</RemainingTime>
+      <DepartureTime>
+        09:39 <span>発</span>
+      </DepartureTime>
+    </CountdownWrapper>
   );
 };
 
@@ -22,6 +20,8 @@ const CountdownWrapper = styled.div`
   justify-content: center;
   width: 60vw;
   height: 60vw;
+  max-width: 400px;
+  max-height: 400px;
   background-color: ${WHITE};
   border: 10px solid ${BLUE};
   border-radius: 80px;
@@ -40,17 +40,19 @@ const Description = styled.p`
 const RemainingTime = styled.h1`
   font-family: 'Arial';
   font-weight: 700;
-  font-size: 100px;
-  margin: 0 0 15px;
+  font-size: 80px;
+  line-height: 100%;
+  margin: 0 0 10px;
 `;
 
 const DepartureTime = styled.div`
   background-color: #f0f5fb;
-  padding: 12px 10px;
+  padding: 10px 10px;
   font-weight: 700;
-  font-size: 26px;
+  font-size: 22px;
   color: ${BLACK};
   width: 80%;
+  max-width: 205px;
   text-align: center;
   border-radius: 9999px;
   span {

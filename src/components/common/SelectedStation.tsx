380b9378daf5f957a6b43b5dfe0f93fdefe3c6c0
taisei yamashita <taisei6514@gmail.com>
2022-04-22 17:46:08 +0900

    ホームの初期設定作成

---
+++ src/components/common/SelectedStation.tsx
@@ -0,0 +1,9 @@
+const SelectedStation = () => {
+  return (
+    <div>
+      <h1>SelectedStation</h1>
+    </div>
+  );
+};
+
+export default SelectedStation;
--- src/components/page/Home/Home.tsx
+++ src/components/page/Home/Home.tsx
@@ -1,7 +1,15 @@
+import SelectedStation from '@/components/common/SelectedStation';
+
 export const Home = () => {
   return (
     <>
       <h1>Home!!</h1>
+      {/* TODO: 駅名 */}
+      <div>
+        {/* otherStation */}
+        <SelectedStation />
+        {/* otherStation */}
+      </div>
     </>
   );
 };

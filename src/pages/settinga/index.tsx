2b215d61c05348666cce25c669bfd8d9b5f7a2c8
taisei yamashita <taisei6514@gmail.com>
2022-04-23 00:29:20 +0900

    buildでのエラーの解消

--- src/pages/settinga/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next';
-import { Setting } from '@/components/page/settings';
-
-const SettingPage: NextPage = () => {
-  return <Setting />;
-};
-
-export default SettingPage;

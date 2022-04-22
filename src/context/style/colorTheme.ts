1fb3cef9398067396faf2a532e90bf42e794a65f
taisei yamashita <taisei6514@gmail.com>
2022-04-22 18:06:47 +0900

    カラーテーマの追加

---
+++ src/context/style/colorTheme.ts
@@ -0,0 +1,8 @@
+export const BLUE: string = '#6C9BD2';
+export const BLACK: string = '#17191E';
+export const WHITE: string = '#fff';
+export const RED: string = '#D16E6B';
+export const YELLOW: string = '#CED16B';
+export const GREEN: string = '#9AD16B';
+export const ORANGE: string = '#D1A16B';
+export const GRAY: string = '#B9BABC';

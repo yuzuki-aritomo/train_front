d9b806e7da424c88e0baa5a3eed1fba3bdb925a5
Tatsu-tatsu <tatsu.progra7@gmail.com>
2022-04-23 18:07:19 +0900

    propsデフォルト値の追加

--- src/components/common/settings/SettingListItem.tsx
+++ src/components/common/settings/SettingListItem.tsx
@@ -5,25 +5,25 @@ import { GRAY } from "@/context/style/colorTheme";
 
 type SettingListItemProps = {
   settingName: string,
-  hasBorderBottom: boolean,
+  isLastChild?: boolean,
 }
 
-export const SettingListItem: React.FC<SettingListItemProps> = (props) => {
+export const SettingListItem: React.FC<SettingListItemProps> = ({settingName, isLastChild = false}) => {
   return (
-    <SettingListItemWrapper hasBorderBottom={props.hasBorderBottom}>
-      <SettingListItemName>{props.settingName}</SettingListItemName>
+    <SettingListItemWrapper isLastChild={isLastChild}>
+      <SettingListItemName>{settingName}</SettingListItemName>
       <Image alt="arrow_right" src={ArrowRight}/>
     </SettingListItemWrapper>
   )
 }
 
-const SettingListItemWrapper = styled.div<{hasBorderBottom: boolean}>`
+const SettingListItemWrapper = styled.div<{isLastChild: boolean}>`
   width: 92%;
   padding:16px 0;
   border-bottom: ${(props) => 
-    props.hasBorderBottom ? `1px solid ${GRAY}`: `none`};
+    props.isLastChild ? `none` : `1px solid ${GRAY}`};
   margin: ${(props) => 
-    props.hasBorderBottom ? `0 auto 10px auto`: `0 auto`};
+    props.isLastChild ? `0 auto` : `0 auto 10px auto`};
   display: flex;
   justify-content: space-between;
   align-items: center;
--- src/components/pages/settings/Settings.tsx
+++ src/components/pages/settings/Settings.tsx
@@ -6,16 +6,16 @@ export const Settings = () => {
   return (
     <SettingContainer>
       <SettingListItemsWrapper>
-        <SettingListItem hasBorderBottom={true} settingName="駅を変更" />
-        <SettingListItem hasBorderBottom={false} settingName="パスワードを変更" />
+        <SettingListItem settingName="駅を変更" />
+        <SettingListItem isLastChild settingName="パスワードを変更" />
       </SettingListItemsWrapper>
       <SettingListItemsWrapper>
-        <SettingListItem hasBorderBottom={true} settingName="利用規約" />
-        <SettingListItem hasBorderBottom={true} settingName="プライバシーポリシー" />
-        <SettingListItem hasBorderBottom={false} settingName="お問い合わせ" />
+        <SettingListItem settingName="利用規約" />
+        <SettingListItem settingName="プライバシーポリシー" />
+        <SettingListItem isLastChild settingName="お問い合わせ" />
       </SettingListItemsWrapper>
       <SettingListItemsWrapper>
-        <SettingListItem hasBorderBottom={false} settingName="ログアウト" />
+        <SettingListItem isLastChild settingName="ログアウト" />
       </SettingListItemsWrapper>
     </SettingContainer>
   );
@@ -30,7 +30,6 @@ const SettingContainer = styled.div`
 const SettingListItemsWrapper = styled.div`
   width: 100%;
   font-weight: 700;
-  text-align: center;
   background-color: ${WHITE};
   padding: 10px 0;
   margin: 0 0 20px 0;

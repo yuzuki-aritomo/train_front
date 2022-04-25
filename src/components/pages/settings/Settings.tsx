b77baa0723cdacd256ee91796e91c0cb0275f8c7
Tatsu-tatsu <tatsu.progra7@gmail.com>
2022-04-25 18:55:07 +0900

    デザイン修正

--- src/components/pages/settings/Settings.tsx
+++ src/components/pages/settings/Settings.tsx
@@ -6,33 +6,34 @@ import { GRAY, WHITE } from '@/context/style/colorTheme';
 
 export const Settings = () => {
   return (
-    <SettingContainer>
-      <SettingHeader pageName="設定" />
-      <SettingListItemsWrapper>
-        <SettingListItem name="駅を変更" route="/settings/change-stations" />
-        <SettingListItem isLastChild name="パスワードを変更" route="/" />
-      </SettingListItemsWrapper>
-      <SettingListItemsWrapper>
-        <SettingListItem name="利用規約" route="/" />
-        <SettingListItem name="プライバシーポリシー" route="/" />
-        <SettingListItem isLastChild name="お問い合わせ" route="/" />
-      </SettingListItemsWrapper>
-      <SettingListItemsWrapper>
-        <SettingListItem isLastChild name="ログアウト" route="/" />
-      </SettingListItemsWrapper>
+    <>
+      <SettingContainer>
+        <SettingHeader pageName="設定" />
+        <SettingListItemsWrapper>
+          <SettingListItem name="駅を変更" route="/settings/change-stations" />
+          <SettingListItem isLastChild name="パスワードを変更" route="/" />
+        </SettingListItemsWrapper>
+        <SettingListItemsWrapper>
+          <SettingListItem name="利用規約" route="/" />
+          <SettingListItem name="プライバシーポリシー" route="/" />
+          <SettingListItem isLastChild name="お問い合わせ" route="/" />
+        </SettingListItemsWrapper>
+        <SettingListItemsWrapper>
+          <SettingListItem isLastChild name="ログアウト" route="/" />
+        </SettingListItemsWrapper>
+      </SettingContainer>
       <IconMenu isHomePage={false} />
-    </SettingContainer>
+    </>
   );
 };
 
 const SettingContainer = styled.div`
-  width: 100vw;
+  margin 0 auth;
   height: 100vh;
   background-color: ${GRAY};
 `;
 
 const SettingListItemsWrapper = styled.div`
-  width: 100%;
   font-weight: 700;
   background-color: ${WHITE};
   padding: 10px 0;
